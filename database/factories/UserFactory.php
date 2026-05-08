b2eeb4da03b4716361f8d2cad119333246f0660b
masfajri <fajri@amikom.ac.id>
2026-05-08 08:09:02 +0700

    edit faketory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,8 +25,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+           // 'name' => fake()->name(),
+           'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
